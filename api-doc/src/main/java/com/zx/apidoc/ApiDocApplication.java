7b5fce4e76b9efb7f1b27f811797e0f7795e6187
zhanglexiang <584032306@qq.com>
2022-08-30 20:59:18 +0800

    添加Springfox-swagger入门文档

---
+++ api-doc/src/main/java/com/zx/apidoc/ApiDocApplication.java
@@ -0,0 +1,13 @@
+package com.zx.apidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDocApplication.class, args);
+    }
+
+}
